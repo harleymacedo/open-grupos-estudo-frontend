a425894c2306f4c84fd1bebad2a405f076a57266
harleymacedo <harley.ip@gmail.com>
2022-06-19 19:17:01 -0300

    Tela de Login criada
    Tela de Login com alguns componentes auxiliares.

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return (
+        <div className='container--header'>
+            <h2>Open Grupo de Estudos</h2>
+            <h3>IFCE Campus Crato</h3>
+            <h3>Curso de Sistemas de Informação</h3>
+        </div>
+    )
+}
+
+export default Header;
